c1e7345a7025bb9c72d93a1ec091ffb938b25448
Uncle Drew <2125zyrc.@gmail.com>
2026-05-07 11:53:36 +0800

    fix: 优化

--- pages/src/pages/design/app-configs/getAiView/utils/prompt/index.ts
+++ pages/src/pages/design/app-configs/getAiView/utils/prompt/index.ts
@@ -1,11 +1,13 @@
 import agent from "./agent";
 import designGuide from "./designGuide";
 import developeGuide from "./developeGuide";
+import documentGuide from "./documentGuide";
 
 const promptSections = {
     agent,
     designGuide,
-    developeGuide
+    developeGuide,
+    documentGuide
 }
 
 export default promptSections
