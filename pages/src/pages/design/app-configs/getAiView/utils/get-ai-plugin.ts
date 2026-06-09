dd3d4e94b8f3062ff49458704406f2c43849c6f5
Uncle Drew <2125zyrc.@gmail.com>
2026-06-09 11:30:34 +0800

    chore: 更新版本、模型

--- pages/src/pages/design/app-configs/getAiView/utils/get-ai-plugin.ts
+++ pages/src/pages/design/app-configs/getAiView/utils/get-ai-plugin.ts
@@ -19,19 +19,35 @@ export default ({ user, key }: any) => {
           apiKey: "sk-8M0pxwcucVOECOFhB470Ea9d09444882A8A955F5863194E8",
           models: [
             {
+              id: "qwen3.6-plus",
+              name: "qwen3.6-plus",
+            },
+            {
               id: "kimi-k2.6",
               name: "Kimi-K2.6",
             },
-            {
-              id: "deepseek-v4-flash",
-              name: "deepseek-v4-flash",
-            },
+            // {
+            //   id: "claude-opus-4-8",
+            //   name: "claude-opus-4-8",
+            // },
             {
               id: "deepseek-v4-pro",
               name: "deepseek-v4-pro",
             },
           ],
-        }
+        },
+        // {
+        //   format: "openai",
+        //   providerId: "moonshot",
+        //   baseUrl: "https://api.moonshot.cn/v1/chat/completions",
+        //   apiKey: "sk-HF3CPQB5Ye8c0xBrdivDUDR1snDjHvNyrpqYZxj8YfRSLT7i",
+        //   models: [
+        //     {
+        //       id: "kimi-k2.6",
+        //       name: "Kimi-K2.6",
+        //     }
+        //   ],
+        // }
       ],
     },
     tools: [operateApiTool],
